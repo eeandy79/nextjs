c41d37dda5a02738621887858158c2f1185ffa9d
andy.chang <andy.chang@tfidm.com>
2021-03-22 11:27:28 +0800

    test2

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import '../styles/globals.css'
+import Router from 'next/router';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const onRedirectCallback = (appState) => {
+	Router.replace(appState?.returnTo || '/');
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+	  <Auth0Provider
+        domain={process.env.NEXT_PUBLIC_DOMAIN}
+        clientId={process.env.NEXT_PUBLIC_CLIENT_ID}
+        audience={process.env.NEXT_PUBLIC_AUDIENCE}
+        scope="read:users"
+        redirectUri={typeof window !== 'undefined' && window.location.origin}
+        onRedirectCallback={onRedirectCallback}
+      >
+		<Component {...pageProps} />
+	  </Auth0Provider>
+  );
 }
 
 export default MyApp
