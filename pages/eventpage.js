03f967af56322ec05d02a8a137e4a4ec8adfcd5f
andy.chang <andy.chang@tfidm.com>
2021-04-08 14:36:25 +0800

    [add] eventpage add start/end time

--- pages/eventpage.js
+++ pages/eventpage.js
@@ -7,6 +7,7 @@ import Navbar from "components/Navbars/IndexNavbar.js";
 import Footer from "components/Footers/Footer.js";
 import Editor from "utils/editor.js"
 import ReactHtmlParser from "react-html-parser";
+import moment from "moment";
 
 const proxy = HasuraProxy.getInstance();
 
@@ -14,6 +15,8 @@ export default function EventPage(input) {
   const [iFrame, setIFrame] = useState("");
 	const [editor, setEditor] = useState(null);
   const [title, setTitle] = useState("");
+  const [startTime, setStartTime] = useState(null);
+  const [endTime, setEndTime] = useState(null);
 
   var event_id = input["event_id"]; // todo: handle eventid not valid
 
@@ -26,8 +29,8 @@ export default function EventPage(input) {
           //setEventDetails(_e);
           //console.log(_e);
           setTitle(_e["title"]);
-          //setStartTime(moment(_e["start_datetime"]));
-          //setEndTime(moment(_e["end_datetime"]));
+          setStartTime(moment(_e["start_datetime"]));
+          setEndTime(moment(_e["end_datetime"]));
           setIFrame(_e["video_iframe"]);
           editor.setContents(_e["desc"]);
         });
@@ -44,20 +47,44 @@ export default function EventPage(input) {
     <main>
     <div className="relative pt-16 pb-32 flex flex-col content-center items-center min-h-screen-75">
       <div className="container px-4">
-        <div className="flex py-4">
-          <div className="flex w-full">
+        <div className="flex flex-col lg:flex-row py-4">
+          <div className="flex w-full lg:w-8/12">
             <div className="video">
               { ReactHtmlParser(iFrame) }
             </div>
           </div>
-          <div className="flex md:w-4/12 bg-black">
+          <div className="flex lg:w-4/12 bg-black">
             <div className="flex flex-col">
-              <div className="px-3 mt-3">
-                <span className="start-date-label">
-                  <span className="day">Jul</span>
-                  <span className="day">03</span>
-                </span>
-              </div>
+              <div className="flex flex-row px-3 mt-3">
+                {
+                  (startTime && endTime) ?
+                  (
+                    <>
+                    <div className="start-date-label mr-1">
+                      <span className="day">{startTime.format("MMM")}</span>
+                      <span className="day">{startTime.format("DD")}</span>
+                    </div>
+                    <div className="text-white text-sm mt-1 mr-1">
+                      {startTime.format("HH:mm")} -
+                    </div>
+                    {
+                      // show date bubble if start != end date
+                      startTime.isSame(endTime, 'day') ?
+                      (""):
+                      (
+                        <div className="start-date-label mr-1">
+                          <span className="day">{endTime.format("MMM")}</span>
+                          <span className="day">{endTime.format("DD")}</span>
+                        </div>
+                      )
+                    }
+                    <div className="text-white text-sm mt-1">
+                      {endTime.format("HH:mm")}
+                    </div>
+                    </>
+                  ):("")
+                }
+                </div>
               <div>
                 <h1 className="text-white text-lg px-3 py-3 font-bold">
                   {title}
